d690e60eb8fde953bbe48fbea7371947e312a483
juanjo <201314609@ingenieria.usac.edu.gt>
2018-02-19 22:33:36 -0600

    ventana: principal, Funciones:abrir , guardar , nueva pestaña

---
+++ practica1[lfp]alex/practica1[lfp]alex/Pr.Designer.cs
@@ -0,0 +1,357 @@
+﻿namespace practica1_lfp_alex
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nuevaPestañaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cargarPestañanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarArchivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.compilaciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.analisLexicoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buscarConcidenciasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.archivosDeSalidaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.manualDeUsuarioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.manualTécnicoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.acercaDeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.compilaciónToolStripMenuItem,
+            this.ayudaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(777, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nuevaPestañaToolStripMenuItem,
+            this.cargarPestañanToolStripMenuItem,
+            this.guardarArchivoToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // nuevaPestañaToolStripMenuItem
+            // 
+            this.nuevaPestañaToolStripMenuItem.Name = "nuevaPestañaToolStripMenuItem";
+            this.nuevaPestañaToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.nuevaPestañaToolStripMenuItem.Text = "Nueva Pestaña";
+            this.nuevaPestañaToolStripMenuItem.Click += new System.EventHandler(this.nuevaPestañaToolStripMenuItem_Click);
+            // 
+            // cargarPestañanToolStripMenuItem
+            // 
+            this.cargarPestañanToolStripMenuItem.Name = "cargarPestañanToolStripMenuItem";
+            this.cargarPestañanToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.cargarPestañanToolStripMenuItem.Text = "Cargar Pestaña";
+            this.cargarPestañanToolStripMenuItem.Click += new System.EventHandler(this.cargarPestañanToolStripMenuItem_Click);
+            // 
+            // guardarArchivoToolStripMenuItem
+            // 
+            this.guardarArchivoToolStripMenuItem.Name = "guardarArchivoToolStripMenuItem";
+            this.guardarArchivoToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.guardarArchivoToolStripMenuItem.Text = "Guardar Archivo";
+            this.guardarArchivoToolStripMenuItem.Click += new System.EventHandler(this.guardarArchivoToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            // 
+            // compilaciónToolStripMenuItem
+            // 
+            this.compilaciónToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.analisLexicoToolStripMenuItem,
+            this.buscarConcidenciasToolStripMenuItem,
+            this.archivosDeSalidaToolStripMenuItem});
+            this.compilaciónToolStripMenuItem.Name = "compilaciónToolStripMenuItem";
+            this.compilaciónToolStripMenuItem.Size = new System.Drawing.Size(87, 20);
+            this.compilaciónToolStripMenuItem.Text = "Compilación";
+            // 
+            // analisLexicoToolStripMenuItem
+            // 
+            this.analisLexicoToolStripMenuItem.Name = "analisLexicoToolStripMenuItem";
+            this.analisLexicoToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.analisLexicoToolStripMenuItem.Text = "Analisis Lexico";
+            // 
+            // buscarConcidenciasToolStripMenuItem
+            // 
+            this.buscarConcidenciasToolStripMenuItem.Name = "buscarConcidenciasToolStripMenuItem";
+            this.buscarConcidenciasToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.buscarConcidenciasToolStripMenuItem.Text = "Buscar Coincidencias";
+            // 
+            // archivosDeSalidaToolStripMenuItem
+            // 
+            this.archivosDeSalidaToolStripMenuItem.Name = "archivosDeSalidaToolStripMenuItem";
+            this.archivosDeSalidaToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.archivosDeSalidaToolStripMenuItem.Text = "Archivos de Salida";
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            this.ayudaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.manualDeUsuarioToolStripMenuItem,
+            this.manualTécnicoToolStripMenuItem,
+            this.acercaDeToolStripMenuItem});
+            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.ayudaToolStripMenuItem.Text = "Ayuda";
+            // 
+            // manualDeUsuarioToolStripMenuItem
+            // 
+            this.manualDeUsuarioToolStripMenuItem.Name = "manualDeUsuarioToolStripMenuItem";
+            this.manualDeUsuarioToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.manualDeUsuarioToolStripMenuItem.Text = "Manual De Usuario";
+            // 
+            // manualTécnicoToolStripMenuItem
+            // 
+            this.manualTécnicoToolStripMenuItem.Name = "manualTécnicoToolStripMenuItem";
+            this.manualTécnicoToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.manualTécnicoToolStripMenuItem.Text = "Manual Técnico";
+            // 
+            // acercaDeToolStripMenuItem
+            // 
+            this.acercaDeToolStripMenuItem.Name = "acercaDeToolStripMenuItem";
+            this.acercaDeToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.acercaDeToolStripMenuItem.Text = "Acerca De..";
+            this.acercaDeToolStripMenuItem.Click += new System.EventHandler(this.acercaDeToolStripMenuItem_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Location = new System.Drawing.Point(12, 27);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(293, 433);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(285, 407);
+            this.tabPage1.TabIndex = 3;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(285, 407);
+            this.textBox1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(285, 407);
+            this.tabPage2.TabIndex = 4;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(0, 0);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(283, 404);
+            this.textBox2.TabIndex = 0;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.textBox3);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(285, 407);
+            this.tabPage3.TabIndex = 5;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(0, 0);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(285, 407);
+            this.textBox3.TabIndex = 0;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.textBox5);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(285, 407);
+            this.tabPage4.TabIndex = 6;
+            this.tabPage4.Text = "tabPage4";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(326, 90);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(419, 214);
+            this.textBox4.TabIndex = 2;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(0, 0);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(285, 407);
+            this.textBox5.TabIndex = 0;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(326, 334);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(419, 122);
+            this.textBox6.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(506, 318);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "porcentaje";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(506, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "tokens";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(777, 472);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Comparador";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nuevaPestañaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cargarPestañanToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem compilaciónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarArchivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem analisLexicoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem buscarConcidenciasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem archivosDeSalidaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem manualDeUsuarioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem manualTécnicoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem acercaDeToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ practica1[lfp]alex/practica1[lfp]alex/Pr.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace practica1_lfp_alex
+{
+    public partial class Form1 : Form
+    {
+        String Texto;
+        int contador = 1;
+        String archivoActual;
+        OpenFileDialog file;
+        SaveFileDialog saveFile1;
+        int indice = 0;
+        string[] a = new string[7];
+        string[] line2 = new string[7];
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void acercaDeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Juan José chitay lux." + "\n" + "201314609" + "\n" + "Lenguajes Formales y de Programación \"B-\" ", "Datos", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void nuevaPestañaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string title = "TabPage " + (tabControl1.TabCount + 1).ToString();
+            TabPage myTabPage = new TabPage(title);
+            tabControl1.TabPages.Add(myTabPage);
+
+         
+            //TextBox mytextbox = new TextBox();
+           // tabControl1.TabPages.Add(mytextbox);
+
+        }
+
+
+
+
+
+        private void cargarPestañanToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+
+            abrir();
+            
+
+        }
+
+        // metodo para abrir los archivos 
+
+        public void abrir()
+        {
+            indice = tabControl1.SelectedIndex;
+
+
+            //busqueda de archivo
+            line2[indice] = "";
+
+            string carga;
+            OpenFileDialog busqueda = new OpenFileDialog();
+            busqueda.Filter = "Text Files (.lfp)|*.lfp|All Files (*.*)|*.*";
+            if (busqueda.ShowDialog() == DialogResult.OK)
+            {
+                carga = busqueda.FileName;
+                a[indice] = carga;
+            }
+
+
+            string line;
+
+
+            // carga de archivo
+            System.IO.StreamReader archivo = new System.IO.StreamReader(a[indice]);
+            while ((line = archivo.ReadLine()) != null)
+            {
+                System.Console.WriteLine(line);
+                line2[indice] = line2[indice] + line + "\r\n";
+                //line3 = line3 + line;
+
+            }
+
+            archivo.Close();
+            System.Console.ReadLine();
+            if (indice == 0) { textBox1.Text = line2[indice]; }
+            if (indice == 1) { textBox2.Text = line2[indice]; }
+            if (indice == 2) { textBox3.Text = line2[indice]; }
+            if (indice == 3) { textBox4.Text = line2[indice]; }
+            if (indice == 4) { textBox5.Text = line2[indice]; }
+            if (indice == 5) { textBox6.Text = line2[indice]; }
+          
+
+
+        }
+
+
+
+        private void guardarArchivoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            indice = tabControl1.SelectedIndex;
+
+            if (a[indice].Equals("0"))
+            {
+                string carga;
+                SaveFileDialog busqueda = new SaveFileDialog();
+                busqueda.Filter = "Text Files (.lfp)|*.lfp|All Files (*.*)|*.*";
+                if (busqueda.ShowDialog() == DialogResult.OK)
+                {
+                    carga = busqueda.FileName;
+                    a[indice] = carga;
+                    Console.WriteLine(a[indice]);
+                }
+            }
+
+            System.IO.StreamWriter archivo = new System.IO.StreamWriter(a[indice]);
+
+            if (indice == 0) { archivo.WriteLine(textBox1.Text); }
+            if (indice == 1) { archivo.WriteLine(textBox2.Text); }
+            if (indice == 2) { archivo.WriteLine(textBox3.Text); }
+            if (indice == 3) { archivo.WriteLine(textBox4.Text); }
+            if (indice == 4) { archivo.WriteLine(textBox5.Text); }
+            if (indice == 5) { archivo.WriteLine(textBox6.Text); }
+            
+
+            archivo.Close();
+            MessageBox.Show("Su archivo se ha guardado correctamente");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
